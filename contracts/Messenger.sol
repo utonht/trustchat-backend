12e29de58ad67707a46d683a41c48e98e3f205f7
kstasi <nastasiakondaurova@gmail.com>
2019-10-26 22:36:45 +0300

    new version

--- contracts/Messenger.sol
+++ contracts/Messenger.sol
@@ -1,33 +1,37 @@
 pragma solidity >=0.4.23 <0.6.0;
 
 contract Messenger {
-    event Message(uint _chatId, uint _msgId, address _from, string _message);
+    event Message(uint _chatId, uint _msgId, address _from, bytes _message);
     event JoinChat(uint _chatId, address _member);
     event Invitation(uint _chatId, address _member);
     
     struct ChatInfo {
         bytes[] publicKeys;
         address[] users;
+        mapping (address => bool) invitations;
     }
     
     mapping(uint => ChatInfo) chats;
     uint public chatCounter;
 
-    function send(uint chatId, uint msgId, string memory message) public {
+    function send(uint chatId, uint msgId, bytes memory message) public {
         emit Message(chatId, msgId, msg.sender, message);
     }
 
-    function openChat(bytes publicKey, address[] initialMemmbers) public {
-        joinChat(chatCounter, publicKey);
+    function openChat(bytes memory publicKey, address[] memory initialMemmbers) public {
         for (uint i = 0; i < initialMemmbers.length; i++) {
+            chats[chatCounter].invitations[initialMemmbers[i]] = true;
             emit Invitation(chatCounter, initialMemmbers[i]);
         }
+        chats[chatCounter].invitations[msg.sender] = true;
+        joinChat(chatCounter, publicKey);
         chatCounter += 1;
     }
     
-    function joinChat(uint chatId, bytes publicKey) public {
-        chats[chatCounter].users.push(msg.sender);
-        chats[chatCounter].publicKeys.push(publicKey);
+    function joinChat(uint chatId, bytes memory publicKey) public {
+        require(chats[chatId].invitations[msg.sender]);
+        chats[chatId].users.push(msg.sender);
+        chats[chatId].publicKeys.push(publicKey);
         emit JoinChat(chatId, msg.sender);
     }
 }
