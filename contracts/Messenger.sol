6f0acc2fbc7945c6d5335a5cbb7ea33cc468e1cc
serh11p <git@serh11p.com>
2019-10-26 13:19:58 +0300

    Deploy & fix contracts

--- contracts/Messenger.sol
+++ contracts/Messenger.sol
@@ -1,10 +1,10 @@
-pragma solidity ^0.4.25;
+pragma solidity >=0.4.23 <0.6.0;
 
 contract Messenger {
     event Message(address _from, address _to, string _message);
     mapping (address => mapping(address => bytes[3])) public publicKeys;
     mapping (bytes32 => MessageTool) public chat;
-    
+
     struct MessageTool {
         mapping(bytes32 => bytes) pablicKeys;
         bytes base;
@@ -14,32 +14,32 @@ contract Messenger {
     function send(address to, string memory message) public {
         emit Message(msg.sender, to, message);
     }
- 
-    function updateConnection(address mate, bytes base, bytes modulo, bytes publicKey) public {
+
+    function updateConnection(address mate, bytes memory base, bytes memory modulo, bytes memory publicKey) public {
         publicKeys[msg.sender][mate] = [base, modulo, publicKey];
     }
-    
-    function getConnection(address mate) public view returns(bytes, bytes, bytes) {
+
+    function getConnection(address mate) public view returns(bytes memory, bytes memory, bytes memory) {
         return (publicKeys[mate][msg.sender][0], publicKeys[mate][msg.sender][1], publicKeys[mate][msg.sender][2]) ;
     }
 
-    function openChat(address[] mates, bytes base, bytes modulo) public returns (bytes32) {
+    function openChat(address[] memory mates, bytes memory base, bytes memory modulo) public returns (bytes32) {
         bytes32 hash = calculateIdentifier(abi.encodePacked(msg.sender), mates);
         chat[hash] = MessageTool(base, modulo);
         return hash;
     }
-    
-    function getChatPubkey(bytes32 chatHash, address[] mates) public view returns (bytes) {
+
+    function getChatPubkey(bytes32 chatHash, address[] memory mates) public view returns (bytes memory) {
         bytes32 hash = calculateIdentifier(new bytes(0), mates);
         return chat[chatHash].pablicKeys[hash];
     }
-    
-    function setChatPubkey(bytes32 chatHash, address[] mates, bytes publicKey) public returns (bytes32 hash) {
+
+    function setChatPubkey(bytes32 chatHash, address[] memory mates, bytes memory publicKey) public returns (bytes32 hash) {
         hash = calculateIdentifier(abi.encodePacked(msg.sender), mates);
         chat[chatHash].pablicKeys[hash] = publicKey;
     }
-    
-    function calculateIdentifier(bytes memory initialIdentifier, address[] mates) public pure returns (bytes32 hash) {
+
+    function calculateIdentifier(bytes memory initialIdentifier, address[] memory mates) public pure returns (bytes32 hash) {
         for (uint i = 0; i < mates.length; i++) {
             initialIdentifier = abi.encodePacked(initialIdentifier, mates[i]);
         }
--- tronbox.js
+++ tronbox.js
@@ -1,34 +1,36 @@
 require("dotenv").config();
 
+console.info(process.env.PK);
+
 module.exports = {
-    networks: {
-        development: {
-            privateKey:
-                "da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0",
-            consume_user_resource_percent: 30,
-            fee_limit: 100000000,
+  networks: {
+    development: {
+      privateKey:
+        "da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0",
+      consume_user_resource_percent: 30,
+      fee_limit: 1e9,
 
-            fullHost: "http://127.0.0.1:9090",
+      fullHost: "http://127.0.0.1:9090",
 
-            network_id: "*"
-        },
-        mainnet: {
-            privateKey: process.env.PK,
-            consume_user_resource_percent: 30,
-            fee_limit: 100000000,
+      network_id: "*"
+    },
+    mainnet: {
+      privateKey: process.env.PK,
+      consume_user_resource_percent: 30,
+      fee_limit: 1e9,
 
-            fullHost: "https://api.trongrid.io",
+      fullHost: "https://api.trongrid.io",
 
-            network_id: "*"
-        },
-        shasta: {
-            privateKey: process.env.PRIVATE_KEY_SHASTA,
-            consume_user_resource_percent: 30,
-            fee_limit: 100000000,
+      network_id: "*"
+    },
+    shasta: {
+      privateKey: process.env.PK,
+      consume_user_resource_percent: 30,
+      fee_limit: 1e9,
 
-            fullHost: "https://api.shasta.trongrid.io",
+      fullHost: "https://api.shasta.trongrid.io",
 
-            network_id: "*"
-        },
+      network_id: "*"
     }
+  }
 };
